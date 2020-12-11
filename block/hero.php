b9f2a386cf0b2577f35342b678557d2edbbbb68e
Bola <bolanaguib@gmail.com>
2020-12-11 22:53:35 +0200

    [Added] New Block

--- block/hero.php
+++ block/hero.php
@@ -33,3 +33,35 @@
     </div>
 
 </section>
+
+
+
+<!-- TimeLine Section -->
+
+<section class="h-screen px-2">
+    <div class="grid lg:grid-cols-5 h-full gap-2 lg:gap-0">
+        <?php for ($i = 0; $i < 5; $i++) : ?>
+            <a href="" class="relative group overflow-hidden">
+                <div class="z-30 bg-black w-full h-full absolute opacity-75 transform group-hover:opacity-0 transition ease-in-out duration-500">
+                </div>
+                <img loading="lazy" class="object-cover w-full h-full absolute z-20 transform group-hover:scale-110 transition ease-in-out duration-500" src="<?php echo get_template_directory_uri() ?>/assets/img/x1.jpg" alt="" title="wp3704688">
+                <div class="z-40 relative h-full flex items-center justify-center text-white">
+                    <span class="text-2xl group-hover:-translate-y-20 transform transition duration-500 ease-in-out group-hover:opacity-0  ">Main <b>Preview</b></span>
+                </div>
+                <div class="bg-white hidden lg:block text-gray-700 text-sm  absolute text-center py-16 bottom-0 translate-y-full group-hover:-translate-y-1 left-0 w-full z-50 transform transition duration-500 ease-in-out">
+                    <span>Suitable for any food related timelines / blogs </span>
+                    <br>
+                    <span>(doesn't show all available features)</span>
+                    <div class="flex items-center justify-center text-main mt-4">
+                        <svg class="h-16" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                        </svg>
+                    </div>
+
+                    <br>
+                </div>
+            </a>
+        <?php endfor; ?>
+
+    </div>
+</section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,9 @@ module.exports = {
   variants: {
     rotate: ['active', 'group-hover'],
     scale: ['active', 'group-hover'],
+    opacity: ['active', 'group-hover'],
+    translate: ['active', 'group-hover'],
+    inset: ['active', 'group-hover'],
   },
   plugins: [require('@tailwindcss/typography')],
 };
