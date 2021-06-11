57c3636a35297d2da84c914ab7f24e7e566598d4
Bola <bolanaguib@gmail.com>
2021-06-11 15:14:09 +0200

    eee

--- block/categories.php
+++ block/categories.php
@@ -16,7 +16,7 @@
 
                             <span class="bg-main inline-block absolute top-0 left-0 w-full h-full transform z-0  rotate-3 transition duration-300 ease-in-out group-hover:rotate-0 rounded rounded-lg  "></span>
                             <div class='bg-white relative h-full group-hover:bg-black relative overflow-hidden p-2 rounded rounded-lg z-10 shadow shadow-lg text-center'>
-                                <img class="group-hover:scale-105 transform  transition duration-300 ease-in-out  rounded rounded-lg" src="<?php echo $image['url'] ? $image['url'] : get_template_directory_uri() ?>'/assets/img/T2_5.jpg'  ?>" alt="">
+                                <img class="group-hover:scale-105 transform  transition duration-300 ease-in-out  rounded rounded-lg" src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt'] ?>">
                                 <span style="width: 96%;" class="absolute rounded rounded-lg block  left-0 bottom-0 text-center bg-gradient-to-t from-gray-900 m-2 text-white pb-4 pt-24">
                                 </span>
                                 <span class="block w-full absolute rounded rounded-lg block  left-0 bottom-0 mb-4 text-white text-center"><?php echo $page['title'] ?></span>
